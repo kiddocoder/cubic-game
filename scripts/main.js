9ba647161bcbe719df1302e25072c6089ed336dd
kiddocoder <ndayikezatresor9@gmail.com>
2024-08-19 00:26:30 +0200

    update

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+
+
+document.querySelector('.get-started').addEventListener('mouseover', function() {
+      this.style.transform = 'scale(1.1) translateY(-3px)';
+  });
+  document.querySelector('.get-started').addEventListener('mouseout', function() {
+      this.style.transform = 'scale(1) translateY(0)';
+  });
